3aa70d4f9e015199ac3416bf2d443505fbc4674c
bfruzsi <bfruzsina@stud.uni-obuda.hu>
2023-03-28 21:41:45 +0200

    plotting deleted

--- GYAK/GYAK05/GYAK05.py
+++ GYAK/GYAK05/GYAK05.py
@@ -52,7 +52,6 @@ class KNNClassifier():
 
     def confusion_matrix(self):
         conf_matrix = confusion_matrix(self.y_test, self.y_preds)
-        sns.heatmap(conf_matrix, annot = True)
         return conf_matrix
     
     @property
